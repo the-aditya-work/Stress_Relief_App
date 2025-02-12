9f20ff337687b3eb3b9c26330347765f9d740663
Aditya Rai <aditya.22scse1012774@galgotiasuniversity.edu.in>
2025-02-12 16:47:37 +0530

    Add Tab and also create home screen page

---
+++ Stress_Relief_App.swiftpm/MemoryJarScreen.swift
@@ -0,0 +1,14 @@
+//
+//  MemoryJarScreen.swift
+//  Stress_Relief_App
+//
+//  Created by Aditya Rai on 12/02/25.
+//
+import SwiftUI
+import PhotosUI
+
+struct MemoryJarScreen: View {
+    var body: some View {
+        Text("Memory Jar Screen")
+    }
+}
