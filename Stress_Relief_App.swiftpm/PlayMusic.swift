39e7cb2145eb82f4fc19e822b08ec9d37b8f4328
Aditya Rai <aditya.22scse1012774@galgotiasuniversity.edu.in>
2025-02-14 14:41:50 +0530

    playmusic add in our app

---
+++ Stress_Relief_App.swiftpm/PlayMusic.swift
@@ -0,0 +1,13 @@
+//
+//  PlayMusic.swift
+//  Stress_Relief_App
+//
+//  Created by Batch -2  on 14/02/25.
+//
+import SwiftUI
+
+struct PlayMusic: View {
+    var body: some View {
+        Text("PlayMusic")
+    }
+}
